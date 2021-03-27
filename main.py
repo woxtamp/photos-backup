f2c255f7c9ec2913eb4c549c3bcc5d4e3e1d3838
Sergey Tissen <earnedgravy@gmail.com>
2021-03-27 19:14:23 +0700

    Add checks for input photos count, add warning about more photos count than exist

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ from vkapi import VkApiUser
 VK_TOKEN = '958eb5d439726565e9333aa30e50e0f937ee432e927f0dbd541c541887d919a7c56f95c04217915c32008'
 VK_USER_ID = '552934290'
 VK_API_VERSION = '5.130'
-PHOTOS_COUNT = 5
+PHOTOS_COUNT = 11
 
-vkuser = VkApiUser(VK_TOKEN, VK_API_VERSION)
-vkuser.get_photos(VK_USER_ID, 'profile', PHOTOS_COUNT)
+if PHOTOS_COUNT == 0:
+    print('Ошибка! Вы не можете сохранить 0 фотографий!')
+if not str(PHOTOS_COUNT).isdecimal():
+    print("Ошибка! Введено не число или число не целое!")
+else:
+    vkuser = VkApiUser(VK_TOKEN, VK_API_VERSION)
+    vkuser.get_photos(VK_USER_ID, 'profile', PHOTOS_COUNT)
--- vkapi.py
+++ vkapi.py
@@ -10,7 +10,7 @@ class VkApiUser:
     def __init__(self, token, version):
         self.token = token
         if self.token == '':
-            print('Ошибка! Не указан Вконтакте!')
+            print('Ошибка! Не указан токен Вконтакте!')
 
         self.version = version
         if self.version == '':
@@ -34,49 +34,47 @@ class VkApiUser:
         response = requests.get(getphotos_url, params={**self.params, **getphotos_params})
         if response.status_code == 200 and 'error' not in response.json():
             real_count = response.json()['response']['count']
-            print(f'В выбранном альбоме {real_count} фотографий')
+            print(f'В выбранном альбоме {real_count} фото.')
             sizes_dict = {}
             urls_dict = {}
             output_data = []
-            if photos_count <= real_count:
-                while getphotos_params['offset'] <= photos_count - 1:
-                    for items in response.json()['response']['items']:
-                        time.sleep(0.33)
-                        if str(items['likes']['count']) + '.jpg' not in sizes_dict and str(
-                                items['likes']['count']) + ' ' + str(datetime.datetime.fromtimestamp(items['date'])) + '.jpg' not in sizes_dict:
-                            height = 0
-                            max_size_type = ''
-                            max_size_url = ''
-                            for size in items['sizes']:
-                                if size['height'] == 0:
-                                    max_size_type = size['type']
-                                    max_size_url = size['url']
-                                elif height < size['height']:
-                                    height = size['height']
-                                    max_size_type = size['type']
-                                    max_size_url = size['url']
-                            sizes_dict[str(items['likes']['count']) + '.jpg'] = max_size_type
-                            urls_dict[str(items['likes']['count']) + '.jpg'] = max_size_url
-                        elif str(items['likes']['count']) + '.jpg' in sizes_dict:
-                            height = 0
-                            max_size_type = ''
-                            max_size_url = ''
-                            for size in items['sizes']:
-                                if size['height'] == 0:
-                                    max_size_type = size['type']
-                                    max_size_url = size['url']
-                                if height < size['height']:
-                                    height = size['height']
-                                    max_size_type = size['type']
-                                    max_size_url = size['url']
-                            sizes_dict[str(items['likes']['count']) + ' ' + str(datetime.datetime.fromtimestamp(items['date'])) + '.jpg'] = max_size_type
-                            urls_dict[str(items['likes']['count']) + ' ' + str(datetime.datetime.fromtimestamp(items['date'])) + '.jpg'] = max_size_url
-                    getphotos_params['offset'] += 1
-                    response = requests.get(getphotos_url, params={**self.params, **getphotos_params})
-            else:
-                print('Вы хотите сохранить больше фотографий, чем есть в альбоме!')
-            print(sizes_dict)
-            print(urls_dict)
+            if photos_count >= real_count:
+                print(f'Вы хотите сохранить больше фотографий, чем есть в альбоме {album_id}. Будет сохранено только {real_count} фото.')
+                photos_count = real_count
+            while getphotos_params['offset'] <= photos_count - 1:
+                for items in response.json()['response']['items']:
+                    time.sleep(0.33)
+                    if str(items['likes']['count']) + '.jpg' not in sizes_dict and str(
+                            items['likes']['count']) + ' ' + str(datetime.datetime.fromtimestamp(items['date'])) + '.jpg' not in sizes_dict:
+                        height = 0
+                        max_size_type = ''
+                        max_size_url = ''
+                        for size in items['sizes']:
+                            if size['height'] == 0:
+                                max_size_type = size['type']
+                                max_size_url = size['url']
+                            elif height < size['height']:
+                                height = size['height']
+                                max_size_type = size['type']
+                                max_size_url = size['url']
+                        sizes_dict[str(items['likes']['count']) + '.jpg'] = max_size_type
+                        urls_dict[str(items['likes']['count']) + '.jpg'] = max_size_url
+                    elif str(items['likes']['count']) + '.jpg' in sizes_dict:
+                        height = 0
+                        max_size_type = ''
+                        max_size_url = ''
+                        for size in items['sizes']:
+                            if size['height'] == 0:
+                                max_size_type = size['type']
+                                max_size_url = size['url']
+                            if height < size['height']:
+                                height = size['height']
+                                max_size_type = size['type']
+                                max_size_url = size['url']
+                        sizes_dict[str(items['likes']['count']) + ' ' + str(datetime.datetime.fromtimestamp(items['date'])) + '.jpg'] = max_size_type
+                        urls_dict[str(items['likes']['count']) + ' ' + str(datetime.datetime.fromtimestamp(items['date'])) + '.jpg'] = max_size_url
+                getphotos_params['offset'] += 1
+                response = requests.get(getphotos_url, params={**self.params, **getphotos_params})
             output_dict = {}
             with open('output.json', 'w') as file:
                 for item in sizes_dict:
